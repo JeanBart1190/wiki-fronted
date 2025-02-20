abaa5de33c9cbbee543cb110e403367ac4b34c70
Vincent <20222005026@m.scnu.edu.cn>
2025-02-20 16:57:53 +0800

    删除无用文件

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div  style="color">
+    你好你好你好你好你好你好你好你好你好
+  </div>
 </template>
